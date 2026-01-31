009fa5a881d018ad43571caa56466a9aaeabb173
anshkunj <meghag2075@gmail.com>
2026-01-31 22:43:36 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,8 +1,9 @@
-from fastapi import WebSocket, WebSocketDisconnect
+from typing import List
+from fastapi import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
