beae92c39c02645c929a8f4b14b5426c035867f4
anshkunj <meghag2075@gmail.com>
2026-02-01 00:11:38 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,33 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
     password = Column(String)
+
     score = Column(Integer, default=0)
     wins = Column(Integer, default=0)
 
+
 class Game(Base):
     __tablename__ = "games"
+
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     game_type = Column(String)
     secret = Column(String)
-    attempts = Column(Integer, default=0)
     lives = Column(Integer)
-    status = Column(String, default="active")
+    attempts = Column(Integer)
+    status = Column(String)
+
+
+class Chat(Base):
+    __tablename__ = "chat"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    message = Column(String)
